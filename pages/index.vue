ee03389ae4d87c17f6b83fa88ce371af722367ef
Tyler Mercer <acct@tylermercer.net>
2019-05-04 09:56:50 -0600

    Make helloworld fancier

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <template lang="html">
-  <h1>Hello World!</h1>
+  <div class="main">
+    <logo/>
+    <h1>Hello World!</h1>
+  </div>
 </template>
 
 <script>
+import Logo from '~/components/Logo.vue'
+
 export default {
+  components: {
+    Logo
+  }
 }
 </script>
 
 <style lang="css" scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 100vh;
+}
 </style>

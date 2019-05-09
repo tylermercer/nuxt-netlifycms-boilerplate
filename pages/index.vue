4f6c87a6bd8d8133d7cd24514f9a5c35634ddaa2
Tyler Mercer <acct@tylermercer.net>
2019-05-08 22:43:14 -0600

    Add date to test list

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="main">
     <logo/>
     <h1>Hello World!</h1>
-    <p v-for="post in blogPosts">{{post.title}}</p>
+    <p v-for="post in blogPosts">{{post.title}}, {{post.date}}</p>
   </div>
 </template>
 
